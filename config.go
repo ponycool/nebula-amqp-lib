4efd62755000840022091a22839dcd60acdd477b
Pony <pony@ponycool.com>
2023-06-05 16:27:28 +0800

    Create config.go

---
+++ config.go
@@ -0,0 +1,10 @@
+package nebula_amqp_lib
+
+type Config struct {
+	Driver  Driver `json:"driver"`
+	Enabled bool   `json:"enabled"`
+	Host    string `json:"host"`
+	Port    int    `json:"port"`
+	User    string `json:"user"`
+	Pwd     string `json:"pwd"`
+}

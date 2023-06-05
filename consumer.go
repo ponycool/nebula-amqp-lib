031c425f4eee905aea3bd315ed5e2711e1a7399b
Pony <pony@ponycool.com>
2023-06-05 16:27:35 +0800

    Create consumer.go

---
+++ consumer.go
@@ -0,0 +1,31 @@
+package nebula_amqp_lib
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"go.uber.org/zap"
+)
+
+type Consumer struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+	Tag     string
+	logger  *zap.Logger
+	Done    chan error
+}
+
+func (c *Consumer) Shutdown() error {
+	// will close() the deliveries channel
+	if err := c.Channel.Cancel(c.Tag, true); err != nil {
+		return fmt.Errorf("consumer cancel failed: %s", err)
+	}
+
+	if err := c.Conn.Close(); err != nil {
+		return fmt.Errorf("AMQP connection close error: %s", err)
+	}
+
+	defer c.logger.Info("AMQP shutdown OK")
+
+	// wait for handle() to exit
+	return <-c.Done
+}
